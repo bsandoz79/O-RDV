631e94b0394a28a014a152aeadd973d1a958e37e
Sandoz <bsandoz79@gmail.com>
2026-03-26 12:39:27 +0100

    test: implémentation du smoke test pour App

---
+++ frontend/src/App.test.js
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.js';
+
+test('le composant App s\'affiche sans crash', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+});
