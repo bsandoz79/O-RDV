ad8cd37ea49e7e576f65e8a9e78a29c7c1922960
Sandoz <bsandoz79@gmail.com>
2026-03-26 12:41:28 +0100

    chore: renommage en .jsx pour supprimer les alertes éditeur

--- frontend/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import App from './App.js';
-
-test('vérifie que le projet démarre sans erreur', () => {
-  render(<App />);
-});
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App.js';
+import App from './App';
 
 test('le composant App s\'affiche sans crash', () => {
   render(
