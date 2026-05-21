c78a9e0f5bacd26cf321fa836845a3a3d7386e63
Sandoz <bsandoz79@gmail.com>
2026-05-21 12:36:07 +0200

    fix: logo navbar taille 32px

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -72,7 +72,7 @@ export default function Navbar() {
       <div className="app-navbar__inner">
         {/* Logo */}
         <Link to="/" className="app-navbar__logo">
-          <img src="/logo.png" alt="O'RDV" className="app-navbar__logo-icon" style={{ width: '1em', height: '1em', objectFit: 'contain' }} />
+          <img src="/logo.png" alt="O'RDV" style={{ width: '32px', height: '32px', objectFit: 'contain' }} />
           <span className="app-navbar__logo-text">O'RDV</span>
         </Link>
 
